7732aaed3afe2859e1edac7d650588ee7956d0b9
fedebarbarulo <88630996+fedebarbarulo@users.noreply.github.com>
2021-10-31 20:35:05 -0300

    domingo

--- src/app/reserva/reserva.page.ts
+++ src/app/reserva/reserva.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ReservaService } from './reserva.service';
+import { AlertController } from '@ionic/angular';
 
 
 
@@ -10,14 +11,48 @@ import { ReservaService } from './reserva.service';
 })
 export class ReservaPage implements OnInit {
 
-  public datitosedepilar: any;
-  constructor(private reservaService: ReservaService) { }
+  public datitosdepilar: any;
+  public datitosdesani: any;
+  public sectorbarra: any;
+  public sectorinterior: any;
+  public sectorgaleria: any;
+  public nombreapellido: any;
+  public dni: any;
+  public fecha: any;
+  public datos: any=[];
+
+  constructor(private reservaService: ReservaService, public alertController: AlertController) { }
+  
 
   ngOnInit(){
         this.reservaService.getDatasedes().subscribe(data =>{
           console.log(data);
-          this.datitosedepilar=data;
+          this.datitosdesani=data[0];
+          this.datitosdepilar=data[1];
         })
+    this.reservaService.getDatasectores().subscribe(data =>{
+            console.log(data);
+            this.sectorbarra=data[0];
+            this.sectorinterior=data[1];
+            this.sectorgaleria=data[2];
+          })
       }
+    actualizar() {
+      this.datos.push(this.nombreapellido);
+      this.datos.push(this.dni);
+      this.datos.push(this.fecha);
+      this.reservaService.sendData(this.datos).subscribe(d=>{});
+    }
+    
+    async presentAlert() {
+      const alert = await this.alertController.create({
+        cssClass: 'mialerta',
+        header: '¡Listo!',
+        message: 'Tu Reserva ha sido exitosa',
+        buttons: ['OK']
+      });
+
+      await alert.present();
+    }
   }
       
--- src/app/reserva/reserva.service.ts
+++ src/app/reserva/reserva.service.ts
@@ -10,7 +10,15 @@ export class ReservaService {
   constructor(private http: HttpClient) { }
    
   getDatasedes(){
-    return this.http.get('http://localhost:3001/restaurant_pilar');
+    return this.http.get('http://localhost:3001/restaurant');
+  }
+
+  getDatasectores(){
+    return this.http.get('http://localhost:3001/sector');
+  }
+  
+  sendData(datos){
+    return this.http.post('http://localhost:3001/cargarproyecto', {datos})
   }
     
 }
